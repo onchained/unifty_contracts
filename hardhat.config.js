ee2bd458c3ebd127fb6a83b5e4a88bbd200936f1
Onchained <onchained_eth@protonmail.com>
2021-05-31 13:01:30 -0700

    Create hardhat repo

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  compilers: [
+    {
+      version: "0.5.17"
+    },
+    {
+      version: "0.6.0",
+      settings: { } 
+    }
+  ]
+};
